cde90ffefe2bd48759474870a052b79b7dfc42c4
IevgeniiGlagoliev <glagolev.dev@gmail.com>
2024-08-04 01:36:11 +0300

    finish half project

--- js/main.js
+++ js/main.js
@@ -3,15 +3,20 @@ const embedEngine = {
         embedEngine.binds();
     },
     binds() {
-        const swiper = new Swiper('.swiper', {
-            slidesPerView: 1,
-            calculateHeight:true,
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
+        const checkboxes = document.querySelectorAll('.check-holder input[type="radio"]');
+        const submitButton = document.getElementById('submitButton');
+
+        function toggleButton() {
+            const isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+            submitButton.disabled = !isChecked;
+        }
+
+        checkboxes.forEach(checkbox => {
+            checkbox.addEventListener('change', toggleButton);
         });
+
+        // Initial check in case some checkboxes are pre-checked
+        toggleButton();
     },
 };
 document.addEventListener("DOMContentLoaded", embedEngine.init);
